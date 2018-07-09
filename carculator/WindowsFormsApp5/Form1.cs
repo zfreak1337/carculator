9e50ee2c82dea06b2d87b6e501037778bee2c935
Maxim <kefiro888@gmail.com>
2018-07-09 09:59:49 +0300

    add disign

--- carculator/WindowsFormsApp5/Form1.cs
+++ carculator/WindowsFormsApp5/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp5
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
